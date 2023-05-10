7f4808b5f48bcb6acf09754f2a5eb4be0c190eb8
AndreaMessene <amessene97@gmail.com>
2023-05-10 19:42:19 +0200

    creazione nav statica  con logo e link

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,68 +1,68 @@
 <script >
     export default{
-        name: 'HeaderCompt',
+        name: 'HeaderCompt', 
         data(){
             return{
                 menuLinks: [
 
                     {
                         url:'#',
-                        name:'CHARACTERS',
-                        current:'false'
+                        name:'characters',
+                        current: false
                     },
 
                     {
                         url:'#',
-                        name:'COMICS',
-                        current:'false'
+                        name:'comics',
+                        current: true
                     },
 
                     {
                         url:'#',
-                        name:'MOVIES',
-                        current:'false'
+                        name:'movies',
+                        current: false
                     },
 
                     {
                         url:'#',
-                        name:'TV',
-                        current:'false'
+                        name:'tv',
+                        current: false
                     },
 
                     {
                         url:'#',
-                        name:'GAMES',
-                        current:'false'
+                        name:'games',
+                        current: false
                     },
 
                     {
                         url:'#',
-                        name:'COLLECTIBLES',
-                        current:'false'
+                        name:'collectibles',
+                        current: false
                     },
 
                     {
                         url:'#',
-                        name:'VIDEOS',
-                        current:'false'
+                        name:'videos',
+                        current: false
                     },
 
                     {
                         url:'#',
-                        name:'FANS',
-                        current:'false'
+                        name:'fans',
+                        current: false
                     },
 
                     {
                         url:'#',
-                        name:'NEWS',
-                        current:'false'
+                        name:'news',
+                        current: false
                     },
 
                     {
                         url:'#',
-                        name:'SHOP',
-                        current:'false'
+                        name:'shop',
+                        current: false
                     }
 
                 ]
@@ -81,7 +81,7 @@
                     :key="index"
                     :class="element.current ? 'active' : '' ">
                     <a href="element.url"> {{ element.name}} </a>
-                </li>
+                </li> 
             </ul>
 
         </nav>
@@ -91,5 +91,38 @@
 </template>
 
 <style scoped lang="scss">
-
+    header{
+        nav{
+            height: 100px;
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+            
+            img{
+                height: 60px;
+            }
+            ul{
+                display: flex;
+                li{
+                    list-style: none;
+                    margin: 0 10px;
+                    line-height: 100px;
+                    a{
+                        text-decoration: none;
+                        color: black;
+                        text-transform: uppercase;
+                        font-weight: bold;
+                        font-size: 0.85rem;
+                    }
+                    &.active{
+                        border-bottom: 3px solid blue;
+                        
+                    }
+                    &.active a{
+                        color: blue;
+                    }
+                }
+            }
+        }
+    }
 </style>
